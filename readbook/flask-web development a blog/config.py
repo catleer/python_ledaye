8024b9f527590c2d87be722393c20207fc0633ba
catleer <cquchenle@foxmail.com>
2018-09-05 18:32:04 +0800

    Flask-web开发第一部分学习（1-5章）

---
+++ readbook/flask-web development a blog/config.py
@@ -0,0 +1,9 @@
+"""
+Created by catleer on 2018-05-21.
+"""
+__author__ = 'catleer'
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
---
+++ readbook/flask-web development a blog/docs/02_Jinja基本语法整理.md
@@ -0,0 +1,6 @@
+Jinja地址：http://jinja.pocoo.org/docs/2.10/templates/#builtin-filters
+
+对象引用
+if语句
+for循环
+模板继承
